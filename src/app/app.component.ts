405e4ec55d60efb2e48ae3f988b80603fd8db912
mahi664 <mahighuge664@gmail.com>
2021-05-15 16:31:47 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,21 @@ export class AppComponent {
   uiMessages = [];
   errorMessage;
   states: any = [];
-  topMessage = 'Status of ALL vaccine Availiabiltty will be updated here';
+  districts: any = [];
+  topMessage =
+    'No need to refresh the page it will notify you once the vaccine is available';
+
+  pinCode = '';
+  selectedState = '';
+  selectedDistrict = '';
+  isSearchByPin = true;
+  dateToPass = '';
+
   constructor(private vaccineService: VaccineAppointmentService) {}
+  displaySlots = false;
 
   ngOnInit() {
+    this.createDateToPass();
     this.states = this.vaccineService.getStateList().subscribe(
       response => {
         this.states = response;
@@ -28,30 +39,30 @@ export class AppComponent {
       }
     );
     // setInterval(() => {
-    console.log(
-      new Date().getDate() +
-        '/' +
-        new Date().getMonth() +
-        '/' +
-        new Date().getFullYear() +
-        ' ' +
-        new Date().getHours() +
-        ':' +
-        new Date().getMinutes() +
-        ':' +
-        new Date().getSeconds()
-    );
-    this.vaccineService.getAvailSlots().subscribe(
-      response => {
-        this.vaccineSlots = response;
-        console.log(this.vaccineSlots);
-        this.processData();
-      },
-      error => {
-        this.errorMessage = 'Error in fetching slots: \n' + error;
-        this.topMessage = 'Some thing went wrong please try later';
-      }
-    );
+    // console.log(
+    //   new Date().getDate() +
+    //     '/' +
+    //     new Date().getMonth() +
+    //     '/' +
+    //     new Date().getFullYear() +
+    //     ' ' +
+    //     new Date().getHours() +
+    //     ':' +
+    //     new Date().getMinutes() +
+    //     ':' +
+    //     new Date().getSeconds()
+    // );
+    // this.vaccineService.getAvailSlots().subscribe(
+    //   response => {
+    //     this.vaccineSlots = response;
+    //     console.log(this.vaccineSlots);
+    //     this.processData();
+    //   },
+    //   error => {
+    //     this.errorMessage = 'Error in fetching slots: \n' + error;
+    //     this.topMessage = 'Some thing went wrong please try later';
+    //   }
+    // );
     // }, 4000);
     // this.vaccineService.getAvailSlots().subscribe(response => {
     //   this.vaccineSlots = response;
@@ -89,22 +100,115 @@ export class AppComponent {
             ' Updated on' +
             new Date()
         );
-        // alert(
-        //   'Vaccine Availiable At: ' +
-        //     eachSlot.address +
-        //     ' ' +
-        //     eachSlot.block_name +
-        //     ' ' +
-        //     eachSlot.name +
-        //     ' ' +
-        //     eachSession.available_capacity +
-        //     ' Doses Availiable ' +
-        //     eachSession.date
-        // );
+        alert(
+          'Vaccine Availiable At: ' +
+            eachSlot.address +
+            ' ' +
+            eachSlot.block_name +
+            ' ' +
+            eachSlot.name +
+            ' ' +
+            eachSession.available_capacity +
+            ' Doses Availiable ' +
+            eachSession.date
+        );
       }
     }
     this.uiMessages.push(
       '*******************************************************************************************'
     );
+  }
+
+  changeSearchBy() {
+    this.isSearchByPin = !this.isSearchByPin;
+  }
+
+  populateDistricts() {
+    // alert("populating districts for "+this.selectedState);
+    this.pinCode="";
+    this.vaccineService
+      .populateDistrictForStateIds(this.selectedState)
+      .subscribe(
+        response => {
+          this.districts = response;
+          console.log(this.districts);
+        },
+        error => {
+          console.log('Error in populating districts ' + error);
+        }
+      );
+  }
+
+  fetchAllAvailSlots() {
+    if (this.isSearchByPin) {
+      this;
+      if (this.pinCode === '') {
+        alert('Please enter Pin code !');
+      } else {
+        // alert("Searching for PIN : "+this.pinCode);
+        setInterval(() => {
+          this.vaccineService
+            .getAvailSlotsByPinCode(this.pinCode, this.dateToPass)
+            .subscribe(
+              response => {
+                this.vaccineSlots = response;
+                console.log(this.vaccineSlots);
+                this.processData();
+                this.displaySlots = true;
+              },
+              error => {
+                this.topMessage = 'Something went wrong';
+                console.log('Error in fetching slots' + error);
+              }
+            );
+        }, 4000);
+      }
+    } else {
+      if (this.selectedState === '') {
+        alert('Please select state !');
+      } else if (this.selectedDistrict === '') {
+        alert('Please select District !');
+      } else {
+        // alert(
+        //   'Searching for State : ' +
+        //     this.selectedState +
+        //     ' And District:' +
+        //     this.selectedDistrict
+        // );
+        setInterval(() => {
+          this.vaccineService
+            .getAvailSlotsByDistrict(this.selectedDistrict, this.dateToPass)
+            .subscribe(
+              response => {
+                this.vaccineSlots = response;
+                console.log(this.vaccineSlots);
+                this.processData();
+                this.displaySlots = true;
+              },
+              error => {
+                this.topMessage = 'Something went wrong';
+                console.log('Error in fetching slots' + error);
+              }
+            );
+        }, 4000);
+      }
+    }
+  }
+
+  createDateToPass() {
+    let date: Date;
+    date = new Date();
+
+    if (date.getDate() < 10)
+      this.dateToPass = this.dateToPass + '0' + date.getDate();
+    else this.dateToPass = this.dateToPass + date.getDate();
+
+    let month = date.getMonth() + 1;
+    if (month < 10) this.dateToPass = this.dateToPass + '-' + '0' + month;
+    else this.dateToPass = this.dateToPass + '-' + month;
+
+    this.dateToPass = this.dateToPass + '-' + date.getFullYear();
+
+    console.log('Date to Pass:' + this.dateToPass);
   }
 }
--- src/app/vaccine-appointment.service.ts
+++ src/app/vaccine-appointment.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class VaccineAppointmentService {
+  BASE_URL="https://cdn-api.co-vin.in/api/v2/admin/location";
+  BASE_URL_SLOTS = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public"
   constructor(private http: HttpClient) {}
 
   getAvailSlots() {
@@ -11,7 +13,19 @@ export class VaccineAppointmentService {
     );
   }
 
+  getAvailSlotsByPinCode(pinCode,date){
+    return this.http.get(this.BASE_URL_SLOTS+"/calendarByPin?pincode="+pinCode+"&date="+date);
+  }
+
+  getAvailSlotsByDistrict(districtId,date){
+    return this.http.get(this.BASE_URL_SLOTS+"/calendarByDistrict?district_id="+districtId+"&date="+date);
+  }
+
   getStateList(){
     return this.http.get('https://cdn-api.co-vin.in/api/v2/admin/location/states');
+  }
+
+  populateDistrictForStateIds(stateId){
+    return this.http.get(this.BASE_URL+"/districts/"+stateId);
   }
 }
